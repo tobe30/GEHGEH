d35d6f91b12403ef40204fc28fdcd8b7ef6ae341
Tobe Marizu <tobemarizu@gmail.com>
2025-09-27 13:46:22 +0100

    production settings

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173",              // local dev
-    "https://gehgeh-official.vercel.app/",   // deployed frontend
+    "https://gehgeh-official.vercel.app",   // deployed frontend
   ],
   credentials: true,
 }));
